ce57ac12fb034644f31c7196924e293d8d89a3a4
Lokesh-Soft-Dev <k.lokesh.soft.coder@gmail.com>
2025-11-09 00:00:52 +0530

    feat: add Java Maven project for Jenkins build

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, Jenkins + Maven!");
+        System.out.println("Java application built successfully!");
+        System.out.println("DevOps Task 8 Completed!");
+    }
+}
